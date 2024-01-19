c0426b559fb0337b1585b24ab90e2c9d6d2e6ee5
Nicolas Grilly <nicolas@grilly.com>
2024-01-19 19:43:45 +0100

    Delete unused code

--- src/main.zig
+++ src/main.zig
@@ -121,17 +121,4 @@ pub fn main() !void {
     }
 
     raylib.CloseWindow();
-
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-    _ = stdout; // autofix
-
-    try bw.flush(); // don't forget to flush!
 }
